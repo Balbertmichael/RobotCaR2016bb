8cfe6204f7450bb78bb281df499b9121a781fa82
Balbertmichael <balbertmichael@utexas.edu>
2016-11-29 20:38:52 -0600

    Tuesday Night Two days left

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,16 +1,19 @@
-#define LEFTPHOTO		//These analog input ports need to be defined!
-#define RIGHTPHOTO
-#define CENTERPHOTO
-#define LEFTDISTANCE
-#define RIGHTDISTANCE
-#define CENTERDISTANCE 
+#define LEFTPHOTO	A5	//These analog input ports need to be defined!
+#define RIGHTPHOTO A3
+#define CENTERPHOTO A4
+#define LEFTDISTANCE A2
+#define RIGHTDISTANCE A0
+#define CENTERDISTANCE A1
 #define LEFTOUTPUTA 5
 #define LEFTOUTPUTB 6
 #define RIGHTOUTPUTA 9
 #define RIGHTOUTPUTB 10
 
-int PHOTORESISTOR_THRESHOLD = 250;
-int DISTANCESENSOR_THRESHOLD = 100;
+int LEFTPHOTOREADINGP = 0; //Needs to be defined to white value
+int RIGHTPHOTOREADINGP = 0; //Needs to be defined to white value
+int CENTERPHOTOREADINGP = 0; //Needs to be defined to black value
+int PHOTORESISTOR_THRESHOLD = 370;
+int DISTANCESENSOR_THRESHOLD = 500;
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,13 +38,32 @@ void loop() {
   int LEFTDISTANCEREADING = analogRead(LEFTDISTANCE);
   int RIGHTDISTANCEREADING = analogRead(RIGHTDISTANCE);
   int CENTERDISTANCEREADING = analogRead(CENTERDISTANCE);
-  
-  int LEFTPHOTODIGITAL = 0;							//Default value for all digital readings is 0
-  if (LEFTPHOTOREADING > PHOTORESISTOR_THRESHOLD) { LEFTPHOTODIGITAL = 1; }		//but if analog value > threshold, change to 1
+  Serial.println(" ");
+  Serial.print(LEFTPHOTOREADING);
+  Serial.print("\t");
+  Serial.print(CENTERPHOTOREADING);
+  Serial.print("\t");
+  Serial.print(RIGHTPHOTOREADING);
+  /*int LEFTPHOTODIGITAL = 0;							//Default value for all digital readings is 0
+  if (LEFTPHOTOREADING - LEFTPHOTOREADINGP > PHOTORESISTOR_THRESHOLD) { LEFTPHOTODIGITAL = 1; }		//but if analog value > threshold, change to 1
   int RIGHTPHOTODIGITAL = 0;									
-  if (RIGHTPHOTOREADING > PHOTORESISTOR_THRESHOLD) { RIGHTPHOTODIGITAL = 1; }	// Do they revert back to 0 if < threshold ? - John L
+  if (RIGHTPHOTOREADING - RIGHTPHOTOREADINGP > PHOTORESISTOR_THRESHOLD) { RIGHTPHOTODIGITAL = 1; }	// Do they revert back to 0 if < threshold ? - John L
+  int CENTERPHOTODIGITAL = 0;
+  if (CENTERPHOTOREADING - CENTERPHOTOREADINGP > PHOTORESISTOR_THRESHOLD) { CENTERPHOTODIGITAL = 1; }*/
+  int LEFTPHOTODIGITAL = 0;              //Default value for all digital readings is 0
+  if (LEFTPHOTOREADING > PHOTORESISTOR_THRESHOLD) { LEFTPHOTODIGITAL = 1; }   //but if analog value > threshold, change to 1
+  int RIGHTPHOTODIGITAL = 0;                  
+  if (RIGHTPHOTOREADING > PHOTORESISTOR_THRESHOLD) { RIGHTPHOTODIGITAL = 1; }  // Do they revert back to 0 if < threshold ? - John L
   int CENTERPHOTODIGITAL = 0;
   if (CENTERPHOTOREADING > PHOTORESISTOR_THRESHOLD) { CENTERPHOTODIGITAL = 1; }
+
+  Serial.println(" ");
+  Serial.print(LEFTPHOTODIGITAL);
+  Serial.print(" ");
+  Serial.print(CENTERPHOTODIGITAL);
+  Serial.print(" ");
+  Serial.print(RIGHTPHOTODIGITAL);
+  Serial.print(" ");
 
   int LEFTDISTANCEDIGITAL = 0;							//Same process for distance sensor readings
   if (LEFTDISTANCEREADING > DISTANCESENSOR_THRESHOLD) { LEFTDISTANCEDIGITAL = 1; }
@@ -63,6 +85,11 @@ void loop() {
 		forward();
 	}
   }
+  LEFTPHOTOREADINGP = LEFTPHOTOREADING;
+  RIGHTPHOTOREADINGP = RIGHTPHOTOREADING;
+  CENTERPHOTOREADINGP = CENTERPHOTOREADING;
+
+  //delay(1);
 }
 
 void forward () {
@@ -89,5 +116,5 @@ void stopMotion () {
     digitalWrite(LEFTOUTPUTB, LOW);
     digitalWrite(RIGHTOUTPUTB, LOW);
 }
-}
+
 
